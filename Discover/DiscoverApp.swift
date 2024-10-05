8cc6af3276a7d962be48cb252a6eb67dec609420
Pelangi Masita <pelangimasita18@gmail.com>
2024-10-05 13:20:20 +0700

    Initial Commit

---
+++ Discover/DiscoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoverApp.swift
+//  Discover
+//
+//  Created by Pelangi Masita Wati on 05/10/24.
+//
+
+import SwiftUI
+
+@main
+struct DiscoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
